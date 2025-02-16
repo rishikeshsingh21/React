d5f2c7d239bbc53744dc97076e519aae9086a200
Rishikesh Singh <rishikeshsingh136@gmail.com>
2025-02-16 09:55:56 +0530

    [react && vite ]

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>Hello React | Rishikesh Singh </h1>
+      <p>
+        Learn about how to create the react and project through react and vite{" "}
+      </p>
+      <ul>
+        <li>npx create-react-app fileName --- react</li>
+        <li>npm create vite@latest ---- vite</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
