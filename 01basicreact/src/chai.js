ef6d520671a209434b767b9d25a3b0b48371e23b
Rishikesh <rishikeshsingh136@gmail.com>
2025-12-18 17:22:32 +0530

    [vite]

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,14 @@
+
+
+function Chai() {
+    return (
+        <>
+            <h1>
+                CRA Create React App <br/>
+                npm create vite@latest project-name
+            </h1>
+        </>
+    )
+}
+
+export default Chai;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      <h1>Vite Application</h1>
+      <Chai/>
+    </>
+    
+  )
+}
+
+export default App
